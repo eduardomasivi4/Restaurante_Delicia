05e2ceb6f7f76c2715a4ded88fa5389078102f2e
eduardo-masivi <eduardomasivi4@gmail.com>
2026-01-16 04:09:50 +0100

    estilizando

--- apps/reservas/views.py
+++ apps/reservas/views.py
@@ -1,7 +1,7 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+#from django.http import HttpResponse
 
 # Create your views here.
 
 def criar(request):
-    return HttpResponse('criar reserva testado!')
+    return render (request, 'reservas/criar.html')
