bb6c28b8b137695c97862125806942a80c0bd306
eduardo-masivi <eduardomasivi4@gmail.com>
2026-01-18 00:57:39 +0100

    salvando dados de reserva no banco de dados.

--- apps/reservas/models.py
+++ apps/reservas/models.py
@@ -3,9 +3,12 @@ from phonenumber_field.formfields import PhoneNumberField
 
 class Reserva(models.Model):
     nome= models.CharField(max_length=150)
-    telefone= PhoneNumberField(region='AO')
+    telefone= models.CharField(default=None,max_length=9)
     email= models.EmailField()
     data = models.DateField()
     hora = models.TimeField()
     pessoas= models.IntegerField()
     observacoes= models.CharField(default='Nenhuma',null=False)
+    
+    def __str__(self):
+        return self.nome
--- apps/reservas/views.py
+++ apps/reservas/views.py
@@ -1,26 +1,34 @@
 from django.shortcuts import render
-from .forms import Reserva
+from .forms import Reserva as f_reserva
+from .models import Reserva as tb_reserva
 #from django.http import HttpResponse
 
 def criar(request):
     if request.method == 'POST':
-        form= Reserva(request.POST)
+        form= f_reserva(request.POST)
         
         if form.is_valid():
-            nome= form.cleaned_data('nome')
-            telefone= form.cleaned_data('telefone')
-            email= form.cleaned_data('email')
-            data= form.cleaned_data('data')
-            hora= form.cleaned_data('hora')
-            pessoas= form.cleaned_data('pessoas')
-            observacoes= form.cleaned_data('observacoes')
-            
             # Salar no banco de dados
+            tb_reserva.objects.create(
+                nome= form.cleaned_data['nome'],
+                telefone= form.cleaned_data['telefone'],
+                email= form.cleaned_data['email'],
+                data= form.cleaned_data['data'],
+                hora= form.cleaned_data['hora'],
+                pessoas= form.cleaned_data['pessoas'],
+                observacoes= form.cleaned_data['observacoes'],
+            )
+            
+            # Mostra a tela criar reserva com caixa de mensagem confirmando a criação da reserva
+            return render (request, 'reservas/criar.html', {
+                'form': f_reserva()
+            })
+            
         else:
             return render (request, 'reserva/criar.html',{
                 'form':form
             })
     elif request.method == 'GET':
         return render (request, 'reservas/criar.html', {
-            'form': Reserva()
+            'form': f_reserva()
         })
